ff05a7f899979c708e6332ac9b7ecf3201d9776e
Adrian Utrera Ruiz <adrianutreraruiz@gmail.com>
2021-02-25 17:45:28 +0100

    3. Utilizando las herramientas de refactorización, extrae en un método, que llamarás obtenerNota, las sentencias que se encargan de guardar la calificación. Al hacerlo, deberás cambiar también el nombre del parámetro de entrada y llamarle nota. Vuelve a subir el proyecto con los cambios realizados a GitHub.

--- UtreraRuizAdrian_ENDES_TAREAPRESENCIAL_EV2/src/examenEV2/Notas.java
+++ UtreraRuizAdrian_ENDES_TAREAPRESENCIAL_EV2/src/examenEV2/Notas.java
@@ -6,17 +6,27 @@ public class Notas {
 	 */
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);// Para pedir números por teclado
-		int n = 0;
+		int nota = 0;
 		String calificacion = "";
 		System.out.print("Introduzca una nota: ");
-		n = reader.nextInt();
-		if (n >= 0 && nota < 5)
+		nota = reader.nextInt();
+		calificacion = obtenerNota(nota);
+		System.out.println(calificacion);
+		reader.close();
+	}
+
+	/**
+	 * @param nota
+	 * @return
+	 */
+	private static String obtenerNota(int nota) {
+		String calificacion;
+		if (nota >= 0 && nota < 5)
 			calificacion = "Suspenso";
-		else if (n >= 5 && nota <= 10)
+		else if (nota >= 5 && nota <= 10)
 			calificacion = "Aprobado";
 		else
 			calificacion = "La nota introducida no es correcta";
-		System.out.println(calificacion);
-		reader.close();
+		return calificacion;
 	}
 }
